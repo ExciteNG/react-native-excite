e908d38d420a2e3d56ecf333e75bc0df8a5c001e
ojotuk <ojotuk14@yahoo.com>
2021-07-16 10:12:25 -0700

    added

---
+++ src/theme/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  exciteDark: '#2c2d3f',
+  purple: '#A7CC48',
+  exciteGreen: '#A7CC48',
+  lightGrayDark:'#2C2D3F',
+  lightPurple: '#9388db',
+  lightGrayPurple: '#f7f7fb',
+  pink: '#ff3d69',
+  gray: '#797777',
+}
+
+export default colors
---
+++ src/theme/images.js
@@ -0,0 +1,18 @@
+import { Asset } from 'expo-asset'
+
+// svg
+import Logo from '../../assets/images/logo.svg'
+
+export const svgs = {
+  logo: Logo,
+}
+
+// png/jpeg
+export const images = {
+  logo_sm: require('../../assets/images/logo-sm.png'),
+  logo_lg: require('../../assets/images/logo-lg.png'),
+  market_bg: require('../../assets/images/market.png'),
+}
+
+// image preloading
+export const imageAssets = Object.keys(images).map((key) => Asset.fromModule(images[key]).downloadAsync())
